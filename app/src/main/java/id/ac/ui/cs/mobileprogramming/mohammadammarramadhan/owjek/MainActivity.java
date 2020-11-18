4ec2d5eb5fc335e716826622248292b63f037f1a
Mohammad Ammar Ramadhan <mohammad.ammar61@ui.ac.id>
2020-11-19 05:50:35 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/mohammadammarramadhan/owjek/MainActivity.java
@@ -0,0 +1,27 @@
+package id.ac.ui.cs.mobileprogramming.mohammadammarramadhan.owjek;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import id.ac.ui.cs.mobileprogramming.mohammadammarramadhan.owjek.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        //Create instance of your Fragment
+        Fragment fragment = new MainFragment();
+        //Add Fragment instance to your Activity
+        fragmentTransaction.add(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+}
---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/mohammadammarramadhan/owjek/ui/main/SettingsFragment.java
@@ -0,0 +1,13 @@
+//package id.ac.ui.cs.mobileprogramming.mohammadammarramadhan.owjek.ui.main;
+//
+//import android.os.Bundle;
+//
+//import androidx.preference.PreferenceFragmentCompat;
+//
+//public class SettingsFragment extends PreferenceFragmentCompat {
+//
+//    @Override
+//    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+//        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+//    }
+//}
